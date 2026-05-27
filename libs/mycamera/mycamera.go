5078e8b8a6721298b2596c2602063395f169c0c4
almafa64 <bhatalyak@gmail.com>
2026-05-28 00:26:17 +0200

    made camera goroutines restartable

--- libs/mycamera/mycamera.go
+++ libs/mycamera/mycamera.go
@@ -18,7 +18,19 @@ import (
 	"github.com/vladimirvivien/go4vl/v4l2"
 )
 
-func cameraWorker(id int, config myconfig.CameraModule) {
+var stopChannel chan struct{} = nil
+
+func shouldStop() bool {
+	select {
+	case _, ok := <-stopChannel:
+		// this will only run when channel closes, because there shouldnt be any sending in this channel
+		return !ok
+	default:
+		return false
+	}
+}
+
+func openCamera(camera **device.Device, config myconfig.CameraModule) bool {
 	var format uint32
 	switch config.Format {
 	case "mjpeg":
@@ -37,74 +49,123 @@ func cameraWorker(id int, config myconfig.CameraModule) {
 		Height:      uint32(height),
 	}
 
+	retry := false
 	var err error
+
+	for {
+		if *camera != nil {
+			(*camera).Close()
+			*camera = nil
+		}
+
+		if shouldStop() {
+			return false
+		}
+
+		*camera, err = device.Open(
+			config.Device,
+			device.WithPixFormat(pix_fmt),
+			device.WithFPS(config.Fps),
+		)
+
+		if err == nil {
+			log.Printf("[camera/%s] %s opened", config.Name, config.Device)
+
+			return true
+		} else if !retry {
+			log.Printf("[camera/%s] device open: %s", config.Name, err)
+
+			retry = true
+			time.Sleep(myconst.CAMERA_TRY_TIMEOUT)
+		}
+	}
+}
+
+func startCamera(camera *device.Device, config myconfig.CameraModule) bool {
+	retry := false
+
+	for {
+		camera.Stop()
+
+		if shouldStop() {
+			return false
+		}
+
+		if err := camera.Start(context.Background()); err == nil {
+			log.Printf("[camera/%s] %s connected", config.Name, config.Device)
+
+			return true
+		} else if !retry {
+			log.Printf("[camera/%s] device start: %s", config.Name, err)
+
+			retry = true
+			time.Sleep(myconst.CAMERA_TRY_TIMEOUT)
+		}
+	}
+}
+
+func frameLoop(camera *device.Device, id int) bool {
+	timeoutClock := time.NewTicker(myconst.CAMERA_FRAME_TIMEOUT)
+
+	for {
+		if shouldStop() {
+			return false
+		}
+
+		select {
+		case frame := <-camera.GetFrames():
+			prepmsg, _ := websocket.NewPreparedMessage(websocket.BinaryMessage, append([]byte{byte(id)}, frame.Data...))
+			frame.Release()
+
+			mywebsocket.Clients.Range(func(key, value any) bool {
+				key.(*mystruct.Client).PrepMessageQueue <- prepmsg
+				return true
+			})
+
+			timeoutClock.Reset(myconst.CAMERA_FRAME_TIMEOUT)
+		case <-timeoutClock.C:
+			return true
+		}
+	}
+}
+
+func cameraWorker(id int, config myconfig.CameraModule) {
 	var camera *device.Device
 
 	for {
-		retry := false
-		for {
-			if camera != nil {
-				camera.Close()
-				camera = nil
-			}
-
-			camera, err = device.Open(
-				config.Device,
-				device.WithPixFormat(pix_fmt),
-				device.WithFPS(config.Fps),
-			)
-			if err == nil {
-				retry = false
-				log.Printf("Opened %s", config.Name)
-				break
-			} else if !retry {
-				log.Printf("[camera/%s] device open: %s", config.Name, err)
-				retry = true
-				time.Sleep(myconst.CAMERA_TRY_TIMEOUT)
-			}
+		if !openCamera(&camera, config) {
+			break
 		}
 
-		for {
-			camera.Stop()
-			if err := camera.Start(context.Background()); err == nil {
-				retry = false
-				log.Printf("Started %s", config.Name)
-				break
-			} else if !retry {
-				log.Printf("[camera/%s] device start: %s", config.Name, err)
-				retry = true
-				time.Sleep(myconst.CAMERA_TRY_TIMEOUT)
-			}
+		if !startCamera(camera, config) {
+			break
 		}
 
-		timeoutClock := time.NewTicker(myconst.CAMERA_FRAME_TIMEOUT)
-
-		log.Printf("[camera/%s] %s connected", config.Name, config.Device)
-	FRAMELOOP:
-		for {
-			select {
-			case frame := <-camera.GetFrames():
-				prepmsg, _ := websocket.NewPreparedMessage(websocket.BinaryMessage, append([]byte{byte(id)}, frame.Data...))
-
-				mywebsocket.Clients.Range(func(key, value any) bool {
-					key.(*mystruct.Client).PrepMessageQueue <- prepmsg
-					return true
-				})
-				timeoutClock.Reset(myconst.CAMERA_FRAME_TIMEOUT)
-
-				frame.Release()
-			case <-timeoutClock.C:
-				break FRAMELOOP
-			}
+		if !frameLoop(camera, id) {
+			break
 		}
+
 		log.Printf("[camera/%s] %s disconnected", config.Name, config.Device)
+		camera.Close()
+	}
+
+	if camera != nil {
+		log.Printf("[camera/%s] %s exited", config.Name, config.Device)
 		camera.Close()
 	}
 }
 
-// TODO: stop all camera goroutine when this called
 func InitCamera() {
 	cameraCounter := 0
+
+	if stopChannel != nil {
+		close(stopChannel)
+		time.Sleep(myconst.CAMERA_FRAME_TIMEOUT * 2) // leave time for goroutines to exit
+	}
+
+	stopChannel = make(chan struct{})
+
+	log.Printf("[camera] initialising cameras")
 
 	for _, segment := range myconfig.Get().Segments {
 		for _, module := range segment {
@@ -112,6 +173,7 @@ func InitCamera() {
 			if !ok {
 				continue
 			}
+
 			go cameraWorker(cameraCounter, camera)
 			cameraCounter++
 		}
